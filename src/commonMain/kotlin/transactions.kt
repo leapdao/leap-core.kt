d8acf71d2eaa02c800e081f129f3fe9ba81e31b6
Jan <jan@parseclabs.org>
2019-11-11 07:52:16 +0100

    Fix tx type location.

--- src/commonMain/kotlin/transactions.kt
+++ src/commonMain/kotlin/transactions.kt
@@ -4,55 +4,78 @@ import leapcore.lib.encode.*
 import leapcore.lib.decode.*
 
 sealed class Transaction : Serializable {
-    abstract val type: UByte
+//    companion object : Decoder<Transaction> by
+//            GetUByte bind {type ->
+//            when(type) {
+//                Transfer.type -> Transfer
+//                else -> Transfer
+//            } }
 }
 
 data class Transfer(val inputs: List<SignedInput>, val outputs: List<Output>) : Transaction() {
-    override val type = 3.toUByte(); // will be ignored by data class methods, problem? Maybe on copy type wont get copied?
     override fun toByteArray(): ByteArray = type.toByteArray() + inputs.toByteArray() + outputs.toByteArray()
 
     companion object : Decoder<Transfer> by
-            TODO()
+            GetUByte.map(::parseLengths) bind { pair ->
+            val ins = pair.first
+            val outs = pair.second
+            mul(SignedInput, ins)        bind { inputs ->
+            mul(Output, outs)            bind { outputs ->
+                pure(Transfer(inputs, outputs))
+            } } }
+    {
+        val type = 3.toUByte()
+    }
 }
 
 data class SpendingCondition(val inputs: List<SignedInput>, val outputs: List<Output>): Transaction() {
-    override val type: UByte = 13.toUByte()
     override fun toByteArray(): ByteArray = TODO()
 
     companion object : Decoder<SpendingCondition> by
             TODO()
+    {
+        val type: UByte = 13.toUByte()
+    }
 }
 
 data class Deposit(val depositId: UInt, val output: Output) : Transaction() {
-    override val type = 2.toUByte();
     override fun toByteArray(): ByteArray = TODO()
 
     companion object : Decoder<Deposit> by
             TODO()
+    {
+        val type = 2.toUByte()
+    }
 }
 
 data class EpochLength(val epochLength: UInt) : Transaction() {
-    override val type: UByte = 12.toUByte()
     override fun toByteArray(): ByteArray = TODO()
 
     companion object : Decoder<EpochLength> by
             TODO()
+    {
+        val type: UByte = 12.toUByte()
+    }
 }
 
 data class MinGasPrice(val minGasPrice: ByteArray): Transaction() {
-    override val type: UByte = 14.toUByte()
     override fun toByteArray(): ByteArray = TODO()
 
     companion object : Decoder<MinGasPrice> by
             TODO()
+    {
+        val type: UByte = 14.toUByte()
+    }
 }
 
 data class ValidatorJoin(val slotId: UShort, val tendermintKey: Bytes32, val eventsCount: UInt, val ethAddress: Bytes32): Transaction() {
-    override val type: UByte = 8.toUByte()
     override fun toByteArray(): ByteArray = TODO()
 
     companion object : Decoder<ValidatorJoin> by
             TODO()
+    {
+        val type: UByte = 8.toUByte()
+    }
 }
 
 data class ValidatorLogout(val slotId: UShort,
@@ -60,26 +83,32 @@ data class ValidatorLogout(val slotId: UShort,
                            val eventsCount: UInt,
                            val activationEpoch: UInt,
                            val newSigner: Bytes32): Transaction() {
-    override val type: UByte = 9.toUByte()
     override fun toByteArray(): ByteArray = TODO()
 
     companion object : Decoder<ValidatorLogout> by
             TODO()
+    {
+        val type: UByte = 9.toUByte()
+    }
 }
 
 data class PeriodVote(val slotId: UShort): Transaction() {
-    override val type: UByte = 11.toUByte()
     override fun toByteArray(): ByteArray = TODO()
 
     companion object : Decoder<PeriodVote> by
             TODO()
+    {
+        val type: UByte = 11.toUByte()
+    }
 }
 
 data class Exit(val input: Input): Transaction() {
-    override val type: UByte = 7.toUByte()
     override fun toByteArray(): ByteArray = TODO()
 
     companion object : Decoder<Exit> by
             TODO()
+    {
+        val type: UByte = 7.toUByte()
+    }
 }
 
